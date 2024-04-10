4356a2c3aa555845a7741fd55e862aa13f7ccc98
Aleksandra Batanova <medwedizaa@gmail.com>
2024-04-10 17:54:32 +0200

    Added button logic

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,19 +2,28 @@ package com.example.calculator
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(R.layout.activity_main)  {
 
+    private var clickCount: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val textView: TextView = findViewById(R.id.my_text)
-        textView.text = "Мой новый текст"
+        textView.text = "Кнопка была нажата $clickCount раз"
 
         textView.setOnClickListener {
-            Log.i("MY_TAG", "My message")
+            Log.e("MY_TAG", "My message")
+        }
+
+        val button: Button = findViewById(R.id.increment_button)
+        button.setOnClickListener {
+            clickCount++
+            textView.text = "Кнопка была нажата $clickCount раз"
         }
     }
 }
